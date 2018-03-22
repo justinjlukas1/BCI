baa701858f8425d09bbc68224047062e744c14ad
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-03-21 21:57:22 -0500

    Created app

---
+++ app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.justinlukas.cs491;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    http://profandroid.com/network/bluetooth/connecting-via-usb.html
+//    https://developer.android.com/guide/topics/connectivity/bluetooth.html
+//    https://www.androidauthority.com/adding-bluetooth-to-your-app-742538/
+}
