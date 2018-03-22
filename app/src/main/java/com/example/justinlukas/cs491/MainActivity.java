ffa9398f95362e6a5e7ec6e51d727b6b2291bc9e
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-03-22 10:27:28 -0500

    updated bluetooth stuff

--- app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
+++ app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
@@ -13,6 +13,13 @@ package com.example.justinlukas.cs491;
 //
 ////    https://developer.android.com/guide/topics/connectivity/bluetooth.html
 ////    https://www.androidauthority.com/adding-bluetooth-to-your-app-742538/
+
+
+//      for transferring node.js files through bluetooth
+//      https://github.com/eelcocramer/node-bluetooth-serial-port
+//
+//      https://stackoverflow.com/questions/6369585/how-to-connect-with-paired-bluetooth-device-programmatic-in-android
+
 //}
 
 import android.app.Activity;
@@ -36,6 +43,7 @@ public class MainActivity extends Activity  {
     private BluetoothAdapter BA;
     private Set<BluetoothDevice>pairedDevices;
     ListView lv;
+    int ENABLE_BT_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +56,38 @@ public class MainActivity extends Activity  {
         b4=(Button)findViewById(R.id.button4);
 
         BA = BluetoothAdapter.getDefaultAdapter();
+        if (BA == null) {
+
+        //Display a toast notifying the user that their device doesn’t support Bluetooth//
+
+            Toast.makeText(getApplicationContext(),"This device doesn’t support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"This device does support Bluetooth",Toast.LENGTH_SHORT).show();
+            //this is where we need to do all the listeners and things?
+        }
+
+//If BluetoothAdapter doesn’t return null, then the device does support Bluetooth
+
         lv = (ListView)findViewById(R.id.listView);
     }
 
     public void on(View v){
+        if (!BA.isEnabled()) {
+
+            //Create an intent with the ACTION_REQUEST_ENABLE action, which we’ll use to display our system Activity//
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            //Pass this intent to startActivityForResult(). ENABLE_BT_REQUEST_CODE is a locally defined integer that must be greater than 0,
+            //for example private static final int ENABLE_BT_REQUEST_CODE = 1//
+            startActivityForResult(enableIntent, ENABLE_BT_REQUEST_CODE);
+            Toast.makeText(getApplicationContext(), "Enabling Bluetooth!", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
         if (!BA.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOn, 0);
