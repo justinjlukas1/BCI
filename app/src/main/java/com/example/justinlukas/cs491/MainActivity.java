fe1edf240473afb827ca2b8e79639a5c9edcf696
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-05 12:26:10 -0500

    Not recognizing FireBaseDatabase

--- app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
+++ app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
@@ -9,6 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    myRef.setValue("Hello, World!");
+
 }
+
+
+// existing stimulus in java script
