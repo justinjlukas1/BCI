af585536cf1a59bbc3c7298e9fb5ecd0c6f8aa44
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-05 10:23:27 -0500

    fixed my broke ass code

--- app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
+++ app/src/main/java/com/example/justinlukas/cs491/MainActivity.java
@@ -1,124 +1,14 @@
 package com.example.justinlukas.cs491;
-//
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//
-////    https://developer.android.com/guide/topics/connectivity/bluetooth.html
-////    https://www.androidauthority.com/adding-bluetooth-to-your-app-742538/
 
-
-//      for transferring node.js files through bluetooth
-//      https://github.com/eelcocramer/node-bluetooth-serial-port
-//
-//      https://stackoverflow.com/questions/6369585/how-to-connect-with-paired-bluetooth-device-programmatic-in-android
-
-//}
-
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Set;
-
-public class MainActivity extends Activity  {
-    Button b1,b2,b3,b4;
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice>pairedDevices;
-    ListView lv;
-    int ENABLE_BT_REQUEST_CODE = 1;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b1 = (Button) findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
-        b3=(Button)findViewById(R.id.button3);
-        b4=(Button)findViewById(R.id.button4);
-
-        BA = BluetoothAdapter.getDefaultAdapter();
-        if (BA == null) {
-
-        //Display a toast notifying the user that their device doesn’t support Bluetooth//
-
-            Toast.makeText(getApplicationContext(),"This device doesn’t support Bluetooth",Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(getApplicationContext(),"This device does support Bluetooth",Toast.LENGTH_SHORT).show();
-            //this is where we need to do all the listeners and things?
-        }
-
-//If BluetoothAdapter doesn’t return null, then the device does support Bluetooth
-
-        lv = (ListView)findViewById(R.id.listView);
     }
 
-    public void on(View v){
-        if (!BA.isEnabled()) {
-
-            //Create an intent with the ACTION_REQUEST_ENABLE action, which we’ll use to display our system Activity//
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-
-            //Pass this intent to startActivityForResult(). ENABLE_BT_REQUEST_CODE is a locally defined integer that must be greater than 0,
-            //for example private static final int ENABLE_BT_REQUEST_CODE = 1//
-            startActivityForResult(enableIntent, ENABLE_BT_REQUEST_CODE);
-            Toast.makeText(getApplicationContext(), "Enabling Bluetooth!", Toast.LENGTH_LONG).show();
-        }
-
-
-
-
-
-        if (!BA.isEnabled()) {
-            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnOn, 0);
-            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void off(View v){
-        BA.disable();
-        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
-    }
-
-
-    public  void visible(View v){
-        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(getVisible, 0);
-    }
-
-
-    public void list(View v){
-        pairedDevices = BA.getBondedDevices();
-
-        ArrayList list = new ArrayList();
-
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
-        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
-
-        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-
-        lv.setAdapter(adapter);
-    }
 }
